f56b0cd51852bf465e9e90e84bd144ab8ed9fd45
Pedro Thor <86499958+pedrothor@users.noreply.github.com>
2023-03-02 19:07:36 -0300

    admin.py

--- admin.py
+++ admin.py
@@ -1,17 +1,37 @@
 from django.contrib import admin
-from core.models import Agendamentos, HorariosPadrao, Servicos
+from core.models import Agendamento, HorarioPadrao, Servico, Podologia, ManicurePedicure, Depilacao, Cilios
 
 
-@admin.register(Agendamentos)
+@admin.register(Agendamento)
 class AgendamentosAdmin(admin.ModelAdmin):
     list_display = ('data', 'servico', 'horario', 'descricao', 'data_marcacao', 'atualizado',)
 
 
-@admin.register(HorariosPadrao)
+@admin.register(HorarioPadrao)
 class HorariosPadraoAdmin(admin.ModelAdmin):
     list_display = ('horario',)
 
 
-@admin.register(Servicos)
+@admin.register(Servico)
 class ServicosAdmin(admin.ModelAdmin):
     list_display = ('nome',)
+
+
+@admin.register(Podologia)
+class PodologiaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
+
+
+@admin.register(ManicurePedicure)
+class ManicurePedicureAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
+
+
+@admin.register(Depilacao)
+class DepilacaoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
+
+
+@admin.register(Cilios)
+class CiliosAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'preco')
